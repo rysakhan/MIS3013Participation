fbf0b0241c0d36fb0390ebefa985f0935b9b3c84
Rysa Khan <rysakhansc@ou.edu>
2020-09-08 11:54:37 -0500

    fizz buzz start

---
+++ Participation/FizzBuzzRK/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FizzBuzzRK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Randome();
+            int number = rnd.Next(1, 101);
+            Console.WriteLine(number); 
+        }
+    }
+}
