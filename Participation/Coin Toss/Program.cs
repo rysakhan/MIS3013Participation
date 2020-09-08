12a6d5dce40a3722618972bb27443998f7a691ff
Rysa Khan <rysakhansc@ou.edu>
2020-09-08 12:24:04 -0500

    Added Coin Toss

---
+++ Participation/Coin Toss/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Coin_Toss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 3);
+
+            if (number == 1)
+            {
+                Console.WriteLine("Heads");
+            }
+
+            else
+            {
+                Console.WriteLine("Tails");
+            }
+
+        }
+    }
+}
