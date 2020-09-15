72ca8abca56745ec6445103cdcf67875e3c07feb
Rysa Khan <rysakhansc@ou.edu>
2020-09-15 12:23:49 -0500

    in class example

---
+++ Participation/class915/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace forandwhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-100");
+
+            int sum = 0;
+            int i = 1;
+            //    control  ; var,       increment/decremenation
+            for (i = 1; i <= 100; i++)
+            {
+                
+                sum = sum + i;
+               
+            }
+            Console.WriteLine("Sum is : " + sum);
+
+            int iterations = 1;
+
+            Console.WriteLine("do you want to perform another iteration, yes/no");
+            string response = Console.ReadLine();
+
+            while(response.ToLower()=="yes")
+            {
+                iterations = iterations + 1;
+                Console.WriteLine("do you want to perform another iteration, yes/no");
+                response = Console.ReadLine();
+            }
+            Console.WriteLine("the number of iterations is " + iterations);
+            Console.ReadKey();
+        }
+    }
+}
