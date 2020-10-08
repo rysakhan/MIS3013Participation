9927d07d8e8ba90af2a0dca647fb4d079c14ca9c
Rysa Khan <rysakhansc@ou.edu>
2020-10-07 22:24:18 -0500

    Confused

---
+++ Participation/CollectionsMinMaxAvg/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsMinMaxAvg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<double> Examgrades = new List<double>();
+
+            Console.WriteLine("How many exam grades are there >>>");
+            int NumOfExams = Convert.ToInt32(Console.ReadLine());
+
+            int[] ExamGrades = new int[NumOfExams - 1];
+            int Exam = 0;
+
+            for (int i = 0; i < NumOfExams - 1; i++)
+            {
+                Console.WriteLine("Enter exam grade >>>");
+                 Exam = Convert.ToInt32(Console.ReadLine());
+
+                ExamGrades[i] = new int[Exam];
+            }
+
+            Console.WriteLine(ExamGrades);
+
+        }
+    }
+}
