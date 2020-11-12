4bb688b3bf09095b5bc6517257680391a400739c
Rysa Khan <rysakhansc@ou.edu>
2020-11-12 12:25:50 -0600

    update

--- Participation/Classes_RectAndCirc/Toy.cs
+++ Participation/Classes_RectAndCirc/Toy.cs
@@ -10,9 +10,33 @@ namespace Classes_RectAndCirc
     class Toy
     {
         public string Manufacturer{get; set; }
-
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Notes { get; set; }
+       
         public Toy()
         {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Notes = "";
+        }
+
+        public Toy (string manufacturer, string name, double price, string notes)
+        {
+            Manufacturer = manufacturer;
+            Name = name;
+            Price = price;
+            Notes = notes;
+        }
+
+        public string GetAisle()
+        {
+            char UpperManu = Manufacturer[0].ToUpper();
+            Random rand = new Random();
+            int RandomNumber = rand.Next(0, 24);
+            string result = UpperManu
+
 
         }
 
