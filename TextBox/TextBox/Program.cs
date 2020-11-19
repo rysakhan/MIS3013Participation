e92dc88ad875edf0657d51d55c8ddcc1d5d2567d
Rysa Khan <rysakhansc@ou.edu>
2020-11-19 12:23:41 -0600

    done!

---
+++ TextBox/TextBox/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox myText = new TextBox();
+
+            myText.Padding = 20;
+            myText.BackColor = ConsoleColor.Cyan;
+            myText.ForeColor = ConsoleColor.DarkBlue;
+            myText.Texts = "Hi folks!";
+
+            myText.DisplayText();
+        }
+    }
+}
---
+++ TextBox/TextBox/TextBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Texts { get; set; }
+
+        public TextBox()
+        {
+            Padding = 15;
+            BackColor = ConsoleColor.Red;
+            ForeColor = ConsoleColor.White;
+            Texts = " ";
+
+        }
+
+        public TextBox(int padding, ConsoleColor backcolor, ConsoleColor forecolor, string texts)
+        {
+            Padding = padding;
+            BackColor = backcolor;
+            ForeColor = forecolor;
+            Texts = texts;
+        }
+
+        
+        
+        public void DisplayText()
+        {
+                Texts = Texts.PadLeft(Padding, ' ');
+                Texts = Texts.PadRight(Texts.Length + Padding, ' ');
+
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            //ForeColor = ConsoleColor.Cyan;
+            Console.WriteLine(Texts);
+
+        }
+    }
+}
